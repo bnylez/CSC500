af4379aec38f220b189a751fea1eb031188e70bb
Bilal Nyle Zafar <bnylez@Bilals-MacBook-Air-2.local>
2025-09-14 22:32:08 -0500

    file upload

---
+++ critthinking1/addprogram.py
@@ -0,0 +1,10 @@
+def main():
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+    outputadd = num1 + num2
+    outputsub = num1 - num2
+    print("The sum of the two numbers is:", outputadd)
+    print("The difference of the two numbers is:", outputsub)
+
+if __name__ == "__main__":
+    main()
---
+++ critthinking1/multdivprogram.py
@@ -0,0 +1,10 @@
+def main():
+    num1 = input("Enter the first number: ")
+    num2 = input("Enter the second number: ")
+    outputmul = float(num1) * float(num2)
+    outputdiv = float(num1) / float(num2)
+    print("The product of the two numbers is:", outputmul)
+    print("The quotient of the two numbers is:", outputdiv)
+
+if __name__ == "__main__":
+    main()
